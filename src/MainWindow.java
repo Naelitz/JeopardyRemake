00ee4e3fe9fa2e147a9adb45b232baedf7cdf2de
Naelitz <davidnaelitz@ymail.com>
2015-11-17 11:54:26 -0500

    Initial setup

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javafx.stage.Stage;
+
+/**
+ * Created by david_000 on 11/17/2015.
+ */
+public class MainWindow
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+    }
+
+}
